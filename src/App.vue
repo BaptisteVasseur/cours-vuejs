29a9e3ee33b092badadef270f5ceba870c87a971
Baptiste Vasseur <baptiste.vasseur@calmedica.fr>
2025-04-01 10:49:00 +0200

    :tada: first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="bg-red-500 text-white">
+    Title
+  </header>
+
+  <RouterView />
+
+  <footer>
+    Copyright
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
